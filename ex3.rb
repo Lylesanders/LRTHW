f1b76033e4640ecd29e611ff0d02c4ae7ed885a6
lyle Sanders <lylesanders66@yahoo.com>
2016-08-17 17:31:36 -0400

    LRTHW ex3.rb counting chickens and math

---
+++ ex3.rb
@@ -0,0 +1,23 @@
+puts "I will now count chickens:" # prints to screen
+
+puts "Hens #{25+30/6}" # prints Hens and solves the math problem. 30/6= 5+25
+puts "Roosters #{100-25*3%4}" # prints Roosters and solves the math problem 100- the remainder (25*3)/4= 3
+
+puts "Now I will count the eggs:" #prints to screen
+
+puts 3+2+1-5+4%2-1/4+6 # solves problem to 6.78 according to yahoo and rounds up to 7. mixed numbers need to be ran as floating point or big decimals to display correctly
+
+puts "Is it true 3+2<5-7?" # prints to screen
+
+puts 3+2<5-7 # solves comparison of 5>-2 as true
+
+puts "What is 3+2? #{3+2}" #prints and What is 3+2 to screen and solves
+puts "What is 5-7? #{5-7}" # prints 5-7 to screen and solves
+
+puts "Oh that's why it's false" #prints statement to screen
+
+puts "How about some more." # prints to screen
+
+puts "Is it greater? #{5> -2}"# compares 5> -2 as true without viewing the source code this would be confusing
+puts "Is it greater or equil? #{5>= -2}" # compares that 5>= -2 as true
+puts "Is it less or equil? #{5<=-2}" # compares 5<= -2 as false
