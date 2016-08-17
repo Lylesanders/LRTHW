d521e77daa0e1dbdd70478a16717a7023e44c994
lyle Sanders <lylesanders66@yahoo.com>
2016-08-17 15:13:19 -0400

    lrthw ex2.rb

--- ex1.rb
+++ ex1.rb
@@ -3,5 +3,7 @@ puts "Hello Again"
 puts "I like typing like this."
 puts "This is fun."
 puts "Yay! printing."
-puts "I'd much rather you 'not'."
+#puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.' 
+puts "Hi world, its me again."
+sleep (5)
