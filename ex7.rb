a139a60745689869718fd0296b5f1234babaf4bc
lyle Sanders <lylesanders66@yahoo.com>
2016-08-17 20:07:03 -0400

    LRTHW ex7.rb More Printing

---
+++ ex7.rb
@@ -0,0 +1,21 @@
+puts "Mary had a little lamb." # displays text
+puts "Its fleece was white as #{'snow'}." #displays #{'snow'} as snow
+puts "And everywhere that Mary went." # displays text
+puts "." * 10 # prints 10 dots
+ # assign string values for end1- end 12 for word cheeseburber
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 ="g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line and what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6  # Displays "Cheese"
+puts end7 + end8 + end9 + end10 + end11 + end12 # displays "Burger"
