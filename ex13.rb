b83feec71524de8db0a582b3d55957145ed710c2
lyle Sanders <lylesanders66@yahoo.com>
2016-09-03 10:12:24 -0400

    removed the else statement

--- ex13.rb
+++ ex13.rb
@@ -1,6 +1,6 @@
-first, second, third = ARGV	
+first, second, third = ARGV						# ARGV receives arguments from the command line
 
-if first == nil
+if first == nil									# This checks that an argument was passed
 	prompt ="> "
 	print "first variable #{prompt}"
 	first = $stdin.gets.chomp
@@ -8,12 +8,9 @@ if first == nil
 	second = $stdin.gets.chomp
 	print "third variable #{prompt}"
 	third = $stdin.gets.chomp
-else
-	first = first
-	second = second
-	third = third
+
 end
-puts "Your first variable is: #{first}"
+puts "Your first variable is: #{first}"			# displays argument passed
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
 puts first + second + third
