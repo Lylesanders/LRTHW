58379af7e0e27629a05882e14848136d96d15fa1
lyle Sanders <lylesanders66@yahoo.com>
2016-09-03 12:55:46 -0400

    place get and print functions in a function

--- ex14.rb
+++ ex14.rb
@@ -1,22 +1,26 @@
 user_name = ARGV.first # gets the first argument
-prompt ='>'
+prompt = '> '
 
-puts "Hi #{user_name}."
-puts "I'd like to ask you a few questions. "
-puts "Do you like me #{user_name}? "
-puts prompt
-likes = $stdin.gets.chomp
 
-puts "Where do you live #{user_name}? "
-puts prompt
-lives = $stdin.gets.chomp
 
-# a comma for puts is like using it twice
-puts "Whag kind of computer do you have? ", prompt
-computer = $stdin.gets.chomp
 
-puts"""
-Alright, oyu said #{likes} about liking me.
-you live in #{lives}. Not sure where that is
-And oyu have a #{computer} computer. Nice
-"""
+def state(user_name,prompt)
+	puts "Hi #{user_name}."
+	puts "I'd like to ask you a few questions. "
+	print "Do you like me #{user_name}? ", prompt
+	likes = $stdin.gets.chomp
+
+	print "Where do you live #{user_name}? ", prompt
+	lives = $stdin.gets.chomp
+
+	# a comma for puts is like using it twice
+	print "What kind of computer do you have? ", prompt
+	computer = $stdin.gets.chomp
+
+	puts"""
+	Alright, you said #{likes} about liking me.
+	you live in #{lives}. Not sure where that is
+	And you have a #{computer} computer. Nice
+	"""
+end
+state(user_name,prompt)
