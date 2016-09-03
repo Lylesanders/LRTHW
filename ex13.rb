f8a700d042b44e0a64c0478d1fbf204fecdf1131
lyle Sanders <lylesanders66@yahoo.com>
2016-09-03 09:44:40 -0400

    request if empty string passed

--- ex13.rb
+++ ex13.rb
@@ -1,5 +1,19 @@
 first, second, third = ARGV	
 
+if first == nil
+	prompt ="> "
+	print "first variable #{prompt}"
+	first = $stdin.gets.chomp
+	print "second variable #{prompt}"
+	second = $stdin.gets.chomp
+	print "third variable #{prompt}"
+	third = $stdin.gets.chomp
+else
+	first = first
+	second = second
+	third = third
+end
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
+puts first + second + third
