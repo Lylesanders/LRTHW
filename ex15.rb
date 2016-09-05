17350f5e2d1fcba9d22a98386c507059f47a52a3
lyle Sanders <lylesanders66@yahoo.com>
2016-09-05 13:25:39 -0400

    first run

--- ex15.rb
+++ ex15.rb
@@ -1,14 +1,13 @@
-# filename = ARGV.first					#recieves filename from command line
-print "Enter filename > "
-filename =$stdin.gets.chomp
-txt = open(filename, 'r')					# opens file
+filename = ARGV.first					#recieves filename from command line
+
+txt = open(filename, 'r')				# opens file
 
 puts "Here's your file #{filename}:"
 puts txt.read		 					#prints file to screen
 
-# print "type the filename again: "		# reopens the file
-# file_again = $stdin.gets.chomp
+print "type the filename again: "		# reopens the file
+file_again = $stdin.gets.chomp
 
-# txt_again = open(file_again)
+txt_again = open(file_again)
 
-# print txt_again.read
+print txt_again.read
