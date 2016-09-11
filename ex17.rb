01651550095c7d9b6586cdc6aeff67e1b644fc8c
lyle Sanders <lylesanders66@yahoo.com>
2016-09-11 08:03:03 -0400

    Shortend to 3 lines

--- ex17.rb
+++ ex17.rb
@@ -1,18 +1,3 @@
 from_file, to_file =ARGV
-puts" "
-puts" "
-puts "copying from #{from_file} to #{to_file}"
-
-#we could do these two on one line, how?
-in_file =open(from_file)
-indata = in_file.read
-
-out_file = open(to_file,'w')
-out_file.write(indata)
-
-puts "Completed copying #{indata.length} bytes from #{from_file} to #{to_file}"
-puts" "
-puts" "
-
-out_file.close
-in_file.close
+File.write(to_file, File.read(from_file))
+puts "\n \n Completed copying from  #{from_file} to #{to_file}.\n \n"
